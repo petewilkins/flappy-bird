434715b2d1305dd75507ba9f4cbcf43080d7e866
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-27 20:30:19 +0100

    basic canvas with falling circle

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
-function setup() {
+var bird;
 
+function setup() {
+  createCanvas(400, 600);
+  bird = new Bird();
 }
 
 function draw() {
+  background(0);
+  bird.update();
+  bird.show();
+}
 
+function keyPressed() {
+  if (key == ' ') {
+    console.log("SPACE PRESSED")
+  }
 }
